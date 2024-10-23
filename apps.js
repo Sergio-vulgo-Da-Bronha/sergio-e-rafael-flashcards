bd3521b8b666e4825a96794aacc4a795b87d8ba6
Sergio-vulgo-Da-Bronha <134059952+Sergio-vulgo-Da-Bronha@users.noreply.github.com>
2024-10-23 14:27:58 -0300

    Add files via upload

--- apps.js
+++ apps.js
@@ -10,12 +10,12 @@ function  criarCartao(categoria, pergunta, resposta) {
         cartao.addEventListener('click', viraCartao)
 }
 cartao.innerHTML = `
-<div class=”cartao__conteudo”>
+<div "class="cartao__conteudo">
 <h3>${categoria}</h3>
-<div class=”cartao__conteudo__pergunta”>
+<div "class="cartao__conteudo__pergunta">
 <p>${pergunta}</p>
 </div>
-<div class=”cartao__conteudo__resposta”>
+<div "class="cartao__conteudo__resposta">
 <p>${resposta}</p>
 </div>
 </div>
